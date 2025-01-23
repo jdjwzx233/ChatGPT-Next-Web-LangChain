029a80a543229d57d11c6d48ece7ef3b450d709e
jdjwzx233 <81498925+jdjwzx233@users.noreply.github.com>
2025-01-23 13:24:40 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -314,14 +314,48 @@ const openaiModels = [
   "dall-e-3",
   "o1-mini",
   "o1-preview",
+    "aqa",
+  "chat-bison-001",
+  "embedding-001",
+  "embedding-gecko-001",
+  "gemini-1.0-pro",
+  "gemini-1.0-pro-001",
+  "gemini-1.0-pro-latest",
+  "gemini-1.0-pro-vision-latest",
+  "gemini-1.5-flash",
+  "gemini-1.5-flash-001",
+  "gemini-1.5-flash-001-tuning",
+  "gemini-1.5-flash-002",
+  "gemini-1.5-flash-8b",
+  "gemini-1.5-flash-8b-001",
+  "gemini-1.5-flash-8b-exp-0827",
+    "gemini-1.5-flash-8b-exp-0924",
+  "gemini-1.5-flash-8b-latest",
+  "gemini-1.5-flash-exp-0827",
+  "gemini-1.5-flash-latest",
+  "gemini-1.5-pro",
+  "gemini-1.5-pro-001",
+    "gemini-1.5-pro-002",
+  "gemini-1.5-pro-exp-0801",
+  "gemini-1.5-pro-exp-0827",
+  "gemini-1.5-pro-latest",
+  "gemini-2.0-flash-exp",
+    "gemini-2.0-flash-thinking-exp",
+    "gemini-2.0-flash-thinking-exp-01-21",
+    "gemini-2.0-flash-thinking-exp-1219",
+  "gemini-exp-1114",
+  "gemini-exp-1121",
+    "gemini-exp-1206",
+  "gemini-pro",
+  "gemini-pro-vision",
+  "learnlm-1.5-pro-experimental",
+  "text-bison-001",
+  "text-embedding-004"
+
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
-  "gemini-1.5-pro-latest",
-  "gemini-1.5-flash-latest",
-  "gemini-pro-vision",
-  "gemini-exp-1114",
+
 ];
 
 const anthropicModels = [
